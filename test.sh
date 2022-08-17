ca74d198142d50005a7373f3520f091f32f8b885
Proloy Mishra <67726964+pro465@users.noreply.github.com>
2022-08-17 16:26:39 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,7 @@
 cd implementations/rust
 cargo run --bin ockam node create n1
+cargo run --bin ockam node show n1
 cargo run --bin ockam node stop n1
+cargo run --bin ockam node show n1
 cargo run --bin ockam node start n1
+cargo run --bin ockam node show n1
